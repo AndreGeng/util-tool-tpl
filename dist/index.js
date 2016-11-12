2c7149de0069f7770c114d623c59911f92e8dcc6
GengAndre <gengze@meituan.com>
2016-11-12 11:30:57 +0800

    rename package to add namespace prefix

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,16 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.tpl = tpl;
-function tpl(template, it) {
-  var result = '';
-  var echo = function echo(str) {
-    return result += str;
-  };
+function tpl(template) {
   var outputExp = /<%=(.+?)%>/g;
   var scriptExp = /<%([\s\S]+?)%>/g;
   var script = template.replace(/[\r\n](\s)*/g, '').replace(outputExp, '\'); \n  echo( $1 ); \n  echo(\'').replace(scriptExp, '\'); \n $1 \n  echo(\'');
-  return eval('echo(\'' + script + '\')');
+  return function (script, it) {
+    var result = '';
+    var echo = function echo(str) {
+      return result += str;
+    };
+    eval('echo(\'' + script + '\');');
+    return result;
+  }.bind(null, script);
 }
--- index.es6
+++ index.es6
@@ -1,14 +1,13 @@
-export function tpl(template,){
-  debugger;
+export function tpl(template){
   let outputExp = /<%=(.+?)%>/g;
   let scriptExp = /<%([\s\S]+?)%>/g;
   var script = template.replace(/[\r\n](\s)*/g,'')
     .replace(outputExp, `'); \n  echo( $1 ); \n  echo('`)
     .replace(scriptExp, `'); \n $1 \n  echo('`);
-  return function(it){
+  return (function(script, it){
     let result = '';
     let echo = (str) => result += str;
     eval(`echo('${script}');`);
     return result;
-  }
+  }).bind(null, script);
 }
