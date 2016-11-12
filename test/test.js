fe73229f80fbb4485bedecb4b62b482ac460bb7f
GengAndre <gengze@meituan.com>
2016-11-12 12:04:56 +0800

    add test scripts

--- test/test.js
+++ test/test.js
@@ -1,10 +1,18 @@
-var assert = require('assert');
+var should = require('chai').should();
 import {tpl} from '../index.es6';
 describe('tpl', function() {
   it('should convert template to raw string', function(){
-    var templateStr = '<%= it %>';
-    var data = 'msg';
-    var result = tpl(templateStr)(data);
-    assert.equal(result, data);
+    var templateStr = `
+    <div>
+      <% if(it.show) { %>
+        <%= it.msg %>
+      <% } %>
+    </div>
+    `;
+    var data = {show: true, msg: 'msg'};
+    tpl(templateStr)(data).should.to.contain(data.msg);
+
+    data = {show: false, msg: 'msg'};
+    tpl(templateStr)(data).should.to.not.contain(data.msg);
   });
 });
